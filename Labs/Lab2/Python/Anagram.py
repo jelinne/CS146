19e1c04c8c43c36c2f134bd5e4e8909aee6d3630
jelinne <105076767+jelinne@users.noreply.github.com>
2024-04-28 23:23:30 -0700

    Update Anagram.py

--- Labs/Lab2/Python/Anagram.py
+++ Labs/Lab2/Python/Anagram.py
@@ -1,7 +1,7 @@
 
 class Anagram:
     def anagram(self, s, t):
-        if len(s) != len(t):
+        if len(s) != len(t) or s == t:
             return False
         if sorted(s) == sorted(t):
             return True
